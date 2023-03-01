53a60d9457940e085eb2ccccd229f9e54b53d45c
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-01 22:48:38 +0700

    Add UserActions component

---
+++ src/app/components/layout/components/UserActions.jsx
@@ -0,0 +1,17 @@
+import icons from '@/assets/icons';
+
+import Button from '../../Button';
+
+export default function UserActions() {
+    return (
+        <div className='flex flex-1 justify-end gap-x-2'>
+            <Button leftIcon={icons.search} />
+            <Button leftIcon={icons.user} />
+            <Button leftIcon={icons.cart} className='relative'>
+                <div className='absolute top-0.5 right-[-6px] pt-px w-5 h-5 text-[12px] text-white bg-black rounded-full'>
+                    4
+                </div>
+            </Button>
+        </div>
+    );
+}
