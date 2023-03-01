2dd8dcbd99daa393df6eedcd4e81ac6982f44ad2
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-01 22:44:20 +0700

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,30 @@ module.exports = {
                 red: '#b01a0b',
                 green: '#065f46',
             },
+            keyframes: {
+                'slide-in': {
+                    '0%': { transform: 'translateX(-100%)' },
+                    '100%': { transform: 'translateX(0)' },
+                },
+                'slide-out': {
+                    '0%': { transform: 'translateX(0)' },
+                    '100%': { transform: 'translateX(-100%)' },
+                },
+                'fade-in': {
+                    '0%': { opacity: 0 },
+                    '100%': { opacity: 1 },
+                },
+                'fade-out': {
+                    '0%': { opacity: 1 },
+                    '100%': { opacity: 0 },
+                },
+            },
+            animation: {
+                'slide-in': 'slide-in 0.5s ease-in-out',
+                'slide-out': 'slide-out 0.5s ease-in-out',
+                'fade-in': 'fade-in 0.5s ease-in-out',
+                'fade-out': 'fade-out 0.5s ease-in-out',
+            },
         },
     },
     plugins: [],
