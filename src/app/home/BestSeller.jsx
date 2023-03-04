995894106d150dd18099278d1d7facc0ff822b4c
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-04 14:51:41 +0700

    Rename folders

---
+++ src/app/home/BestSeller.jsx
@@ -0,0 +1,11 @@
+import ProductItem from '../product/ProductItem';
+
+export default function BestSeller() {
+    return (
+        <div className='overflow-x-auto w-screen'>
+            <div>
+                <ProductItem />
+            </div>
+        </div>
+    );
+}
