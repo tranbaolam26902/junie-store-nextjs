5ec3334bb5b73aab2176547fa751da3a098569f8
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-11 17:00:38 +0700

    Add Blog section

---
+++ src/app/home/Blog.jsx
@@ -0,0 +1,42 @@
+import blog1 from '@/assets/images/home/blog-01.webp';
+import blog2 from '@/assets/images/home/blog-02.webp';
+import blog3 from '@/assets/images/home/blog-03.webp';
+
+import SectionHeader from './SectionHeader';
+import BlogItem from './BlogItem';
+
+const posts = [
+    {
+        path: '/',
+        tag: 'Hướng dẫn',
+        title: 'Cách làm sáng hạt đá đơn giản không phải ra hàng',
+        image: blog1,
+    },
+    {
+        path: '/',
+        tag: 'Hướng dẫn',
+        title: 'Những Điều Cần Lưu Ý Khi Đeo Trang Sức Bản To',
+        image: blog2,
+    },
+    {
+        path: '/',
+        tag: 'Hướng dẫn',
+        title: 'Tất Tần Tật Cách Đeo Nhẫn Cưới Các Cặp Đôi Cần Biết',
+        image: blog3,
+    },
+];
+
+export default function Blog() {
+    return (
+        <div className='mx-auto px-6 md:px-10 max-w-screen-2xl'>
+            <SectionHeader subtitle='Bắt kịp xu hướng' title="Junie's Blog" />
+            <div className='overflow-x-auto no-scrollbar mt-12'>
+                <div className='flex justify-center gap-4 md:gap-10 w-[240vw] md:w-[180vw] lg:w-full mx-auto'>
+                    {posts.map((post) => (
+                        <BlogItem post={post} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import About from './home/About';
 import Category from './home/Category';
 import Hero from './home/Hero';
+import Blog from './home/Blog';
 
 export const metadata = {
     title: 'Junie | Thương Hiệu Phụ Kiện Thời Trang Bông Tai, Dây Chuyền Nữ',
@@ -12,6 +13,7 @@ export default function Home() {
             <Hero />
             <Category />
             <About />
+            <Blog />
             <div className='mt-10'></div>
         </>
     );
