17f6162426601540ac00ec8cba11637c4803bb6d
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-13 00:24:21 +0700

    Add overscroll behavior

--- src/app/home/BlogSection.jsx
+++ src/app/home/BlogSection.jsx
@@ -30,7 +30,7 @@ export default function BlogSection() {
     return (
         <div className='mx-auto px-6 md:px-10 max-w-screen-2xl'>
             <SectionHeader subtitle='Bắt kịp xu hướng' title="Junie's Blog" />
-            <div className='overflow-x-auto no-scrollbar mt-12'>
+            <div className='overflow-x-auto no-scrollbar overscroll-contain mt-12'>
                 <div className='flex justify-center gap-4 md:gap-10 w-[240vw] md:w-[180vw] lg:w-full mx-auto'>
                     {posts.map((post) => (
                         <BlogItem post={post} />
--- src/app/home/CategorySection.jsx
+++ src/app/home/CategorySection.jsx
@@ -34,7 +34,7 @@ export default function CategorySection() {
         <div className='mx-auto px-6 md:px-10 max-w-screen-2xl'>
             <SectionHeader subtitle='Tham khảo' title='Danh mục' />
             <p className='mb-12 text-center'>Từ đôi bàn tay của nghệ nhân mang tới cho nàng sự lựa chọn đa dạng.</p>
-            <div className='overflow-x-auto no-scrollbar'>
+            <div className='overflow-x-auto no-scrollbar overscroll-contain'>
                 <div className='flex justify-center gap-4 md:gap-10 w-[160vw] md:w-[110vw] lg:w-full xl:w-10/12 mx-auto'>
                     {categories.map((category, index) => (
                         <CategoryItem key={index} image={category.image} title={category.title} />
