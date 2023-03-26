190cf4e9eb88af4b2c0e20fb83814c1b4b77012d
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-26 15:53:58 +0700

    Add loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,18 @@
+import styles from './loading.module.scss';
+
+export default function Loading() {
+    return (
+        <div className='fixed top-0 left-0 right-0 bottom-0 z-[100] flex items-center justify-center w-screen h-screen bg-white'>
+            <div>
+                <div class={styles['loadingio-spinner-dual-ring-qo59o73r0tl']}>
+                    <div class={styles['ldio-0m5i1demrio']}>
+                        <div></div>
+                        <div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
