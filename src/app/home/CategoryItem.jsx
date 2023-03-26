1191fb9480de16b701e1634076578cb4cc2262c1
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-26 15:04:01 +0700

    Test fake api

--- src/app/home/CategoryItem.jsx
+++ src/app/home/CategoryItem.jsx
@@ -1,19 +1,26 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function CategoryItem({ path, image, title }) {
+import earrings from '@/assets/images/home/earrings.webp';
+
+export default function CategoryItem({ slug, image, name }) {
     return (
-        <Link href='/' className='group'>
+        <Link href={slug} className='group'>
             <div className='relative overflow-hidden rounded-lg'>
-                <Image src={image} alt='category-image' className='invisible' />
+                <Image src={earrings} alt='category-image' className='invisible' />
                 <Image
                     src={image}
+                    fill
+                    sizes='40vw
+                    (max-width: 768px) 30vw
+                    (max-width: 1024px) 25vw
+                    (max-width: 1280px) 20vw'
                     alt='category-image'
                     className='absolute top-0 left-0 right-0 bottom-0 group-hover:scale-105 transition-transform duration-500'
                 />
             </div>
             <p className='mt-6 mx-auto w-fit text-sm font-bold uppercase border-b border-gray transition duration-500 group-hover:border-current'>
-                {title}
+                {name}
             </p>
         </Link>
     );
