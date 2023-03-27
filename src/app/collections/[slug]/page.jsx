8288b541e1ef7664010f2ae97329bf78b84b7280
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-27 23:45:37 +0700

    Add static params

--- src/app/collections/[slug]/page.jsx
+++ src/app/collections/[slug]/page.jsx
@@ -1,30 +1,33 @@
 import { notFound } from 'next/navigation';
 
 import getCollection from '~/app/utils/collections/getCollection';
+import getCollections from '~/app/utils/collections/getCollections';
 
 import HeroSection from './components/HeroSection';
 import ProductsSection from './components/ProductsSection';
 
 export async function generateMetadata({ params }) {
-    const collectionData = await getCollection(params.slug);
+    const collection = await getCollection(params.slug);
 
     return {
-        title: collectionData.id ? collectionData.title : '404 - Không tìm thấy trang',
+        title: collection.id ? collection.title : '404 - Không tìm thấy trang',
     };
 }
 
 export default async function Collection({ params }) {
-    const collectionData = await getCollection(params.slug);
-    if (!collectionData.id) return notFound();
+    const collection = await getCollection(params.slug);
+    if (!collection.id) return notFound();
 
     return (
         <>
-            <HeroSection
-                title={collectionData.title}
-                description={collectionData.description}
-                image={collectionData.image}
-            />
-            <ProductsSection data={collectionData.products} />
+            <HeroSection title={collection.title} description={collection.description} image={collection.image} />
+            <ProductsSection data={collection.products} />
         </>
     );
+}
+
+export async function generateStaticParams() {
+    const collections = await getCollections();
+
+    return collections.map((collection) => ({ slug: collection.id }));
 }
