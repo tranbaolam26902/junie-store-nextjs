f42b888d9301f620c78afba73a9a78c4acf1e835
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-27 23:52:42 +0700

    Fix hero description

--- src/app/collections/[slug]/components/HeroSection.jsx
+++ src/app/collections/[slug]/components/HeroSection.jsx
@@ -22,7 +22,9 @@ export default function HeroSection({ title, description, image }) {
                     <p className='mb-6 md:mb-8 text-3xl md:text-5xl xl:text-6xl capitalize font-bold animate-[slide-in-bottom_400ms_ease-in-out_400ms_both]'>
                         {title}
                     </p>
-                    <p className='animate-[slide-in-bottom_400ms_ease-in-out_600ms_both]'>{description}</p>
+                    <p className='mx-auto px-6 md:px-10 max-w-screen-2xl text-sm md:text-base text-center animate-[slide-in-bottom_400ms_ease-in-out_600ms_both]'>
+                        {description}
+                    </p>
                 </div>
             </div>
         </div>
