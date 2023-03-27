e38461b16768720fecb94232d22e8732c2bf9b2c
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-27 21:23:43 +0700

    Remove cache

--- src/app/utils/home/getFeaturedPosts.jsx
+++ src/app/utils/home/getFeaturedPosts.jsx
@@ -1,5 +1,5 @@
 export default async function getFeaturedPosts() {
-    const res = await fetch('https://junie-store-fake-api.vercel.app/featured-posts');
+    const res = await fetch('https://junie-store-fake-api.vercel.app/featured-posts', { cache: 'no-store' });
 
     return res.json();
 }
