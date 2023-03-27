f117972d9a9c6ae56d7028e0d62f4413fa448c92
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-27 22:55:04 +0700

    Fix generateStaticParams

--- src/app/utils/collections/getCollection.jsx
+++ src/app/utils/collections/getCollection.jsx
@@ -1,5 +1,5 @@
 export default async function getCollection(slug) {
-    const res = await fetch(`https://junie-store-fake-api.vercel.app/${slug}`, { cache: 'no-store' });
+    const res = await fetch(`https://junie-store-fake-api.vercel.app/${slug}`);
 
     return res.json();
 }
