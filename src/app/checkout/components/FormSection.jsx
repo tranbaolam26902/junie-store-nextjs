14d0b498bd22b428c87b677936f2733184b7517d
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-30 15:06:48 +0700

    Add checkout page UI

---
+++ src/app/checkout/components/FormSection.jsx
@@ -0,0 +1,105 @@
+'use client';
+
+import Image from 'next/image';
+
+import arrowLeft from '@/assets/icons/arrow-left.svg';
+
+import Button from '~/app/components/Button';
+import { useRouter } from 'next/navigation';
+
+export default function FormSection() {
+    const router = useRouter();
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmitForm}>
+            <p className='text-2xl mb-8'>Thông tin đơn hàng</p>
+            <div className='flex flex-col gap-4'>
+                <div className='flex flex-col'>
+                    <label htmlFor='phone-number' className='cursor-pointer'>
+                        Số điện thoại
+                    </label>
+                    <input
+                        id='phone-number'
+                        type='number'
+                        placeholder='+84 792 815 452'
+                        className='px-4 pt-3 pb-2.5 text-lg border border-gray rounded'
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='email-address' className='cursor-pointer'>
+                        Email<span className='ml-2 italic text-secondary/70'>(không bắt buộc)</span>
+                    </label>
+                    <input
+                        id='email-address'
+                        type='text'
+                        placeholder='junie-store@gmail.com.vn'
+                        className='px-4 pt-3 pb-2.5 text-lg border border-gray rounded'
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='name' className='cursor-pointer'>
+                        Tên
+                    </label>
+                    <input
+                        id='name'
+                        type='text'
+                        placeholder='Du Phong Linh'
+                        className='px-4 pt-3 pb-2.5 text-lg border border-gray rounded'
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='address' className='cursor-pointer'>
+                        Địa chỉ
+                    </label>
+                    <input
+                        id='address'
+                        type='text'
+                        placeholder='01 Phù Đổng Thiên Vương, Phường 8, TP. Đà Lạt'
+                        className='px-4 pt-3 pb-2.5 text-lg border border-gray rounded'
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='address-detail' className='cursor-pointer'>
+                        Căn hộ, phòng, v.v.<span className='ml-2 italic text-secondary/70'>(không bắt buộc)</span>
+                    </label>
+                    <input
+                        id='address-detail'
+                        type='text'
+                        placeholder='Tầng 01 tòa nhà A27'
+                        className='px-4 pt-3 pb-2.5 text-lg border border-gray rounded'
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='notes' className='cursor-pointer'>
+                        Ghi chú<span className='ml-2 italic text-secondary/70'>(không bắt buộc)</span>
+                    </label>
+                    <textarea
+                        id='notes'
+                        type='text'
+                        rows={4}
+                        placeholder='Giao ngoài giờ hành chính'
+                        className='resize-none px-4 pt-3 pb-2.5 text-lg border border-gray rounded'
+                    />
+                </div>
+            </div>
+            <div className='flex items-center justify-between mt-10'>
+                <Button
+                    leftIcon={arrowLeft}
+                    className='flex gap-2 text-base'
+                    onClick={() => {
+                        router.back();
+                    }}
+                >
+                    Quay lại
+                </Button>
+                <Button secondary type='submit'>
+                    Xác nhận
+                </Button>
+            </div>
+        </form>
+    );
+}
