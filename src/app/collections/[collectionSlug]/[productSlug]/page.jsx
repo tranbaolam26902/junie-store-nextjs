85df2d6735e18013a4c2e94be23b771a851d5d3a
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-04-05 23:53:59 +0700

    Add random products section

--- src/app/collections/[collectionSlug]/[productSlug]/page.jsx
+++ src/app/collections/[collectionSlug]/[productSlug]/page.jsx
@@ -2,13 +2,14 @@
 import { notFound } from 'next/navigation';
 
 // App's features
-import { getProduct, getProducts } from '~/app/services';
+import { getProduct, getProducts, getRandomProducts } from '~/app/services';
 
 // App's components
 import Breadcrumb from './components/Breadcrumb';
 import ProductInfo from './components/ProductInfo';
 import ProductImages from './components/ProductImages';
 import ProductDescription from './components/ProductDescription';
+import ProductsCarousel from '~/app/components/ProductsCarousel';
 
 // Dynamic metadata
 export async function generateMetadata({ params }) {
@@ -23,6 +24,7 @@ export default async function Product({ params }) {
     // Get data
     const product = await getProduct(params.productSlug);
     if (!product) return notFound();
+    const randomProducts = await getRandomProducts(params.collectionSlug, 12, params.productSlug);
 
     return (
         <>
@@ -34,6 +36,18 @@ export default async function Product({ params }) {
                 </div>
                 <div className='grid lg:grid-cols-6'>
                     <ProductDescription product={product} />
+                </div>
+                <div className='mb-16'>
+                    <ProductsCarousel
+                        products={randomProducts}
+                        title='Có thể bạn cũng thích'
+                        subtitle='Lựa chọn khác'
+                        small
+                        xlItemsPerSlide={4}
+                        lgItemsPerSlide={3}
+                        mdItemsPerSlide={2.5}
+                        xsItemsPerSlide={1.5}
+                    />
                 </div>
             </div>
         </>
--- src/app/services/index.jsx
+++ src/app/services/index.jsx
@@ -2,3 +2,4 @@ export { default as getCollection } from './collections/getCollection';
 export { default as getCollections } from './collections/getCollections';
 export { default as getProduct } from './products/getProduct';
 export { default as getProducts } from './products/getProducts';
+export { default as getRandomProducts } from './products/getRandomProducts';
