6d2b8a3afd2ec404f40b84c3b8cd0d69519c2ef9
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-05-02 16:53:35 +0700

    Add search feature

--- src/app/components/layout/components/Search.jsx
+++ src/app/components/layout/components/Search.jsx
@@ -1,22 +1,60 @@
 'use client';
 
 // Third-party libs
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Image from 'next/image';
+
+// styles
+import styles from '~/app/loading.module.scss';
 
 // App's features
 import { selectSearch, setIsClosing, setShowSearch } from '~/redux/features/searchSlice';
 
+// App's hooks
+import useDebounce from '~/app/hooks/useDebounce';
+
+// App's services
+import axios from '~/app/services/api/axios';
+
+// Assets files
 import iconSearch from '@/assets/icons/search.svg';
 import iconClose from '@/assets/icons/close.svg';
 
 // App's components
 import Button from '../../Button';
+import SearchProduct from './SearchProduct';
 
 export default function Search() {
+    // Component's states
+    const [keyword, setKeyword] = useState('');
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isCompleted, setIsCompleted] = useState(false);
+
     // Hooks
     const search = useSelector(selectSearch);
     const dispatch = useDispatch();
+    const debounceKeyword = useDebounce(keyword, 500);
+
+    // Component's functions
+    const searchProduct = (keyword) => {
+        if (keyword.trim() === '') {
+            setProducts([]);
+            return;
+        }
+        setIsLoading(true);
+        setProducts([]);
+        axios
+            .post('/api/products/search', { keyword })
+            .then(({ data }) => {
+                setProducts(data.result);
+            })
+            .finally(() => {
+                setIsLoading(false);
+                setIsCompleted(true);
+            });
+    };
 
     // Component's event handlers
     const handleCloseSearch = () => {
@@ -25,6 +63,10 @@ export default function Search() {
             dispatch(setShowSearch(false));
         }, 200);
     };
+
+    useEffect(() => {
+        searchProduct(keyword);
+    }, [debounceKeyword]);
 
     return (
         <>
@@ -49,15 +91,47 @@ export default function Search() {
                             <Image src={iconSearch} alt='icon-search' />
                         </span>
                         <input
+                            value={keyword}
                             type='text'
                             autoFocus
                             placeholder='Bạn đang muốn tìm kiếm gì?'
                             className='flex-1 border-none outline-none'
+                            onChange={(e) => {
+                                setKeyword(e.target.value);
+                                setIsCompleted(false);
+                            }}
                         />
                     </div>
                     <Button leftIcon={iconClose} iconSize={16} className='mr-[-8px]' onClick={handleCloseSearch} />
                 </div>
                 {/* End: Search header */}
+                {/* Start: Search result */}
+                <div className='overflow-y-auto flex-1 flex flex-col gap-6 p-6 md:px-10'>
+                    {isLoading ? (
+                        <div className='flex items-center justify-center w-full h-full'>
+                            <div className={styles['loadingio-spinner-dual-ring-qo59o73r0tl']}>
+                                <div className={styles['ldio-0m5i1demrio']}>
+                                    <div></div>
+                                    <div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : products.length === 0 && keyword.trim() !== '' && isCompleted ? (
+                        <span>Không tìm thấy sản phẩm</span>
+                    ) : (
+                        products.length > 0 && (
+                            <span>
+                                Tìm thấy <b>{products.length}</b> sản phẩm
+                            </span>
+                        )
+                    )}
+                    {products.map((product, index) => (
+                        <SearchProduct key={index} product={product} />
+                    ))}
+                </div>
+                {/* End: Search result */}
             </div>
         </>
     );
---
+++ src/app/components/layout/components/SearchProduct.jsx
@@ -0,0 +1,53 @@
+// Third-party libs
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Assets files
+import iconArrow from '@/assets/icons/arrow-right.svg';
+import { useDispatch } from 'react-redux';
+import { setIsClosing, setShowSearch } from '~/redux/features/searchSlice';
+
+export default function SearchProduct({ product }) {
+    // Hooks
+    const dispatch = useDispatch();
+
+    return (
+        <Link
+            href={`/collections/${product.collection.slug}/${product.slug}`}
+            className='group relative flex gap-6 items-center'
+            onClick={() => {
+                dispatch(setIsClosing(true));
+                setTimeout(() => {
+                    dispatch(setShowSearch(false));
+                }, 200);
+            }}
+        >
+            <Image src={product.images[0].path} alt='product-image' width={92} height={115} className='rounded' />
+            <div className='flex-1 flex flex-col gap-1'>
+                <span>{product.name}</span>
+                {product.discount ? (
+                    <div className='flex gap-3'>
+                        <span className='text-red'>
+                            {new Intl.NumberFormat('vi-VN').format(product.price * (1 - product.discount))}
+                            <sup className='ml-0.5 underline'>đ</sup>
+                        </span>
+                        <span className='line-through opacity-70'>
+                            {new Intl.NumberFormat('vi-VN').format(product.price)}
+                            <sup className='ml-0.5 underline'>đ</sup>
+                        </span>
+                    </div>
+                ) : (
+                    <span>
+                        {new Intl.NumberFormat('vi-VN').format(product.price)}
+                        <sup>đ</sup>
+                    </span>
+                )}
+            </div>
+            <Image
+                src={iconArrow}
+                alt='icon'
+                className='transition duration-200 translate-x-full opacity-0 group-hover:translate-x-0 group-hover:opacity-100'
+            />
+        </Link>
+    );
+}
